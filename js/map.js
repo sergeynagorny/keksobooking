e2bf20093f3e1d3b14d2aa099d85ab69834fba2d
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-11-16 14:13:19 +0300

    Добавил функции активации / деактивации страницы

--- js/map.js
+++ js/map.js
@@ -1,14 +1,25 @@
 'use strict';
 
 (function () {
-
   var map = document.querySelector('.map');
-  map.classList.remove('map--faded');
+  var pinMain = document.querySelector('.map__pin--main');
 
   var CARD_COUNT = 8;
   var bookingData = window.createBookingData(CARD_COUNT);
 
+  pinMain.addEventListener('mouseup', function () {
+    map.classList.remove('map--faded');
+    activationPage();
+  });
 
-  window.renderOffers(bookingData);
+  var activationPage = function () {
+    window.renderOffers(bookingData);
+    window.form.enable();
+  };
+
+  var deactivationPage = function () {
+    map.classList.add('map--faded');
+    window.form.disabled();
+  };
 
 })();
